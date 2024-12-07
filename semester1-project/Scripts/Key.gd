1e101cef466da7a25d325f2048ac092a37637e20
Lyanair <Rloughran280@gmail.com>
2024-12-07 09:01:18 +0000

    Drums and Basic key script
    Added drum sounds and made it so the key can change between the associated sounds by pressing either the drum or piano button

--- semester1-project/Scripts/Key.gd
+++ semester1-project/Scripts/Key.gd
@@ -1,14 +1,24 @@
 extends Sprite2D
 
 @export var keybind = "a"
-@export var sound: Node
+@export var Pianosound: Node
+@export var Drumsound: Node
+@export var Mouthsound: Node
+var currsound
 
-
+func _on_button_button_down() -> void:
+	currsound = Pianosound
+	print("Piano Button")
+	
+func _on_button_2_button_down() -> void:
+	currsound = Drumsound
+	print("Drum Button")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(keybind):
-		sound.play()
+		currsound.play()
 		self.modulate = Color.RED
+		print("a")
 	
 	if Input.is_action_just_released(keybind):
 		self.modulate = Color.WHITE
